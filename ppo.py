02dd93f922325afc670502ec83b15e47f7aea93d
Deok Gun Park <intuinno@gmail.com>
2023-02-27 19:38:32 -0600

    Second commit

--- ppo.py
+++ ppo.py
@@ -17,12 +17,31 @@ class Agent(nn.Module):
     def __init__(self, envs):
         super(Agent, self).__init__()
         self.critic = nn.Sequential(
-            layer_init(nn.Linear(np.arrary(envs.single_observation_space.shape).prod(), 64)),
+            layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 64)),
             nn.Tanh(),
             layer_init(nn.Linear(64,64)),
             nn.Tanh(),
             layer_init(nn.Linear(64,1), std=1.)
         )
+
+        self.actor = nn.Sequential(
+            layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 64)),
+            nn.Tanh(),
+            layer_init(nn.Linear(64,64)),
+            nn.Tanh(),
+            layer_init(nn.Linear(64,envs.single_action_space.n), std=0.01)
+        )
+        
+    def get_value(self, x):
+        return self.critic(x)
+
+    def get_action_and_value(self, x, action=None):
+        logits = self.actor(x)
+        probs = Categorical(logits=logits)
+        if action is None:
+            action = probs.sample()
+        return action, probs.log_prob(action), probs.entropy(), self.critic(x) 
+
 
 def layer_init(layer, std=np.sqrt(2), bias_const=0):
     torch.nn.init.orthogonal_(layer.weight, std)
@@ -53,8 +72,13 @@ def parse_args():
                         help="the entity (team) of wandb's project")
     parser.add_argument('--num-envs', type=int, default=4, 
                         help='the number of parallel game environment')
+    parser.add_argument('--num-steps', type=int, default=128,
+                        help='the number of steps to run in each environment per policy rollout')
+    parser.add_argument('--anneal-lr', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
+                        help="Toggle learning rate annealing for policy and value networks")
     
     args = parser.parse_args()
+    args.batch_size = int(args.num_envs * args.num_steps)
     return args
 
 def make_env(gym_id, seed, idx, capture_video, run_name):
@@ -105,10 +129,63 @@ if __name__ == "__main__":
     print("envs.single_observation_space.shape: ", envs.single_observation_space.shape)
     print("envs.single_action_space.n", envs.single_action_space.n)
 
-    observation = envs.reset()
-    for _ in range(200):
-        action = envs.action_space.sample()
-        observation, reward, done, info = envs.step(action)
-        for item in info:
-            if "episode" in item.keys():
-                print(f"episodic return {item['episode']['r']}")
+    agent = Agent(envs).to(device)
+    print(agent)
+    optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
+
+    # Storage setup
+    rollout_shape = (args.num_steps, args.num_envs)
+    obs = torch.zeros(rollout_shape + envs.single_observation_space.shape).to(device)
+    actions = torch.zeros(rollout_shape + envs.single_action_space.shape).to(device)
+    logprobs = torch.zeros(rollout_shape).to(device)
+    rewards = torch.zeros(rollout_shape).to(device)
+    dones = torch.zeros(rollout_shape).to(device)
+    values = torch.zeros(rollout_shape).to(device)
+
+    # start the game
+    global_step = 0
+    start_time = time.time()
+    next_obs = torch.Tensor(envs.reset()).to(device)
+    next_done = torch.zeros(args.num_envs).to(device)
+    num_updates = args.total_timesteps // args.batch_size 
+    print(num_updates)
+    print("next_obs.shape: ", next_obs.shape)
+    print("agent.get_value(next_obs):", agent.get_value(next_obs))
+    print("agent.get_value(next_obs).shape:", agent.get_value(next_obs).shape)
+    print("agent.get_action_and_value(next_obs):", agent.get_action_and_value(next_obs))
+
+    for update in range(1, num_updates+1):
+        # anneal learning rate
+        if args.anneal_lr:
+            frac = 1.0 - (update - 1.0) /num_updates
+            lrnow = frac * args.learning_rate
+            optimizer.param_groups[0]['lr'] = lrnow
+
+        for step in range(0, args.num_steps):
+            global_step += 1 * args.num_envs
+            obs[step] = next_obs
+            dones[step] = next_done
+
+            # roll out
+            with torch.no_grad():
+                action, logprob, _, value = agent.get_action_and_value(next_obs)
+                values[step] = value.flatten()
+            actions[step] = action
+            logprobs[step] = logprob 
+
+            # execute action
+            next_obs, reward, done, info = envs.step(action.cpu().numpy())
+            rewards[step] = torch.tensor(reward).to(device).view(-1)
+            next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(next_done).to(device)
+
+            for item in info:
+                if "episode" in item.keys():
+                    print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
+                    writer.add_scalar("charts/episodic_return", item['episode']['r'], global_step)
+                    writer.add_scalar("charts/episodic_length", item['episode']['l'], global_step)
+                    break
+
+
+
+
+    
